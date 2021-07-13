4042755966503bf5021c1a2116a417c7b2f413e9
dlcnc3d <dlcnc3d@gmail.com>
2021-07-13 11:28:23 +0300

    add google map

--- tasktwo/src/components/SidePanel/SidePanel.styles.ts
+++ tasktwo/src/components/SidePanel/SidePanel.styles.ts
@@ -1,52 +1,40 @@
-import {
-    makeStyles,  
-    Theme,
-    createStyles,
-  } from "@material-ui/core/styles";
-  
-  
-  
-  const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-      root: {
-        flexGrow: 1,
-        display: "flex",
-        //flexDirection: "row",    
-        flexDirection: "column",            
-        marginleft: "5px",
-        padding: "5px",
-        justifyContent: "center",
-        //alignItems: "center",
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+      display: "flex",
+      //flexDirection: "row",
+      flexDirection: "column",
+      //marginleft: "5%",
+      //padding: "5px",
+      justifyContent: "center",
+      //alignItems: "center",
+      paddingTop: "7vh",
+      //containerSpacing:"0",
+      width: "100%",
+    },
+    leftPanel: {
+      height: "86vh",
+      backgroundColor: "#a1dffb30",
+      elevation: "100",
+      paddingLeft: "20px",
+    },
+    InputElement: {
+      variant: "outlined",
+      label: "outlined",
+      height: "5vh",
+      fullWidth: "true",
+    },
+    mainForm: {
+      height: "86vh",
+      display: "flex",
+      flexDirection: "column",
+      backgroundImage: "url(https://source.unsplash.com/random)",
+      elevation: "0",
+    },
+  })
+);
 
-                width: "100%",
-      },
-      leftPanel: {
-        height: '90vh', 
-        backgroundColor:"#a1dffb30",
-        elevation:'10',
-        paddingLeft: "20px",
-        //display: "flex",
-        //flexDirection: "row",
-        //alignItems: "stretch",
-
-       
-      },
-      InputElement: {
-        variant:'outlined',
-        label:'outlined',
-        height: '5vh', 
-        fullWidth:'true',
-      },
-      mainForm: {
-        height: '90vh', 
-        backgroundImage:'url(https://source.unsplash.com/random)',
-        elevation:'10',       
-      },
-
-    }
-  ));
-
-  
-  
-  export default useStyles
+export default useStyles;
