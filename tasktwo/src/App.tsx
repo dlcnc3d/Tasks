c35556956d693594c7aa2f67c74ae82df6b1a390
dlcnc3d <dlcnc3d@gmail.com>
2021-07-13 11:14:27 +0300

    add types

--- tasktwo/src/App.tsx
+++ tasktwo/src/App.tsx
@@ -3,26 +3,16 @@ import { defaultTheme } from "./themes/default";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import NavMenu from "./components/NavMenu/NavMenu";
 import { Footer } from "./components/Footer/Footer";
-import { Box } from "@material-ui/core";
 import { SidePanel } from "./components/SidePanel/SidePanel";
-
-
-
-
-
 
 function App() {
   return (
     <MuiThemeProvider theme={defaultTheme}>
       <NavMenu />
-
-      <Box p={0} />
       <CssBaseline />
-
       <main>
         <SidePanel />
       </main>
-
       <Footer year={new Date().getFullYear()}> </Footer>
     </MuiThemeProvider>
   );
